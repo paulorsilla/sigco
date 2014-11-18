b2890b6dbf9147cd95bf796e73374a08e0c3f358
paulorsilla <paulo.silla@gmail.com>
2014-11-18 16:25:09 -0200

    Id na classe empregado.

--- src/br/embrapa/cnpso/sigco/model/Empregado.java
+++ src/br/embrapa/cnpso/sigco/model/Empregado.java
@@ -3,10 +3,16 @@ package br.embrapa.cnpso.sigco.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Empregado implements Serializable {
 
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String matricula;
 	
 }
