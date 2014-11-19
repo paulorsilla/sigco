e66300d50dfda0ac04db11fe5258c0ae94305a13
Lyncoln Mauricio <lyncolnms@gmail.com>
2014-11-19 16:32:54 -0200

    Beans criados: CompTecnica e CompComportamental;

---
+++ src/br/embrapa/cnpso/sigco/controller/CompComportamentalBean.java
@@ -0,0 +1,124 @@
+package br.embrapa.cnpso.sigco.controller;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.primefaces.event.RowEditEvent;
+
+import br.embrapa.cnpso.sigco.model.Comportamental;
+
+@Named
+@Stateful
+@ViewScoped
+public class CompComportamentalBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	private Comportamental comportamental;
+	private Collection<Comportamental> listaComportamental;
+	private List<Comportamental> filtroComportamental;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void init() {
+		this.comportamental = new Comportamental();
+
+		CriteriaQuery cQ = em.getCriteriaBuilder().createQuery();
+		cQ.select(cQ.from(Comportamental.class));
+
+		listaComportamental = em.createQuery(cQ).getResultList();
+	}
+
+	public Comportamental getComportamental() {
+		return comportamental;
+	}
+
+	public void setComportamental(Comportamental comportamental) {
+		this.comportamental = comportamental;
+	}
+
+	public Collection<Comportamental> getListaComportamental() {
+		return listaComportamental;
+	}
+
+	public void setListaComportamental(
+			Collection<Comportamental> listaComportamental) {
+		this.listaComportamental = listaComportamental;
+	}
+
+	public List<Comportamental> getFiltroComportamental() {
+		return filtroComportamental;
+	}
+
+	public void setFiltroComportamental(
+			List<Comportamental> filtroComportamental) {
+		this.filtroComportamental = filtroComportamental;
+	}
+
+	public void salvar(Comportamental comportamental) {
+		try {
+			this.em.persist(comportamental);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.init();
+		}
+	}
+
+	public void excluir(Comportamental comportamental) {
+		try {
+			Comportamental cComportamental = this.em.find(Comportamental.class,
+					this.comportamental.getId());
+			this.em.remove(cComportamental);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.init();
+		}
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		this.comportamental = (Comportamental) event.getObject();
+
+		FacesMessage msg = new FacesMessage(
+				"Competência Comportamental Editado",
+				comportamental.getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		try {
+			this.em.merge(comportamental);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage(
+				"Competência Comportamental Cancelado",
+				((Comportamental) event.getObject()).getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void removeMessage() {
+		FacesMessage msg = new FacesMessage(
+				"Competência Comportamental Removido",
+				comportamental.getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+}
---
+++ src/br/embrapa/cnpso/sigco/controller/CompTecnicaBean.java
@@ -0,0 +1,119 @@
+package br.embrapa.cnpso.sigco.controller;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.primefaces.event.RowEditEvent;
+
+import br.embrapa.cnpso.sigco.model.Tecnica;
+
+@Named
+@Stateful
+@ViewScoped
+public class CompTecnicaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	private Tecnica compTecnica;
+	private Collection<Tecnica> listaTecnica;
+	private List<Tecnica> filtroTecnica;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void Init() {
+		this.compTecnica = new Tecnica();
+
+		CriteriaQuery cQ = em.getCriteriaBuilder().createQuery();
+		cQ.select(cQ.from(Tecnica.class));
+
+		listaTecnica = em.createQuery(cQ).getResultList();
+	}
+
+	public Tecnica getCompTecnica() {
+		return compTecnica;
+	}
+
+	public void setCompTecnica(Tecnica compTecnica) {
+		this.compTecnica = compTecnica;
+	}
+
+	public Collection<Tecnica> getListaTecnica() {
+		return listaTecnica;
+	}
+
+	public void setListaTecnica(Collection<Tecnica> listaTecnica) {
+		this.listaTecnica = listaTecnica;
+	}
+
+	public List<Tecnica> getFiltroTecnica() {
+		return filtroTecnica;
+	}
+
+	public void setFiltroTecnica(List<Tecnica> filtroTecnica) {
+		this.filtroTecnica = filtroTecnica;
+	}
+
+	public void salvar(Tecnica tecnica) {
+		try {
+			this.em.persist(tecnica);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.Init();
+		}
+	}
+
+	public void excluir(Tecnica tecnica) {
+		try {
+			Tecnica cTecnica = this.em.find(Tecnica.class,
+					this.compTecnica.getId());
+			this.em.remove(cTecnica);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.Init();
+		}
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		this.compTecnica = (Tecnica) event.getObject();
+
+		FacesMessage msg = new FacesMessage("Competência Técnica Editado",
+				compTecnica.getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		try {
+			this.em.merge(compTecnica);
+			this.em.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Competência Técnica Cancelado",
+				((Tecnica) event.getObject()).getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void removeMessage() {
+		FacesMessage msg = new FacesMessage("Competência Técnica Removido",
+				compTecnica.getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+}
