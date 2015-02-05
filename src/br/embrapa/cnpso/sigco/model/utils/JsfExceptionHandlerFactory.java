2c088044717e6f2232f8b8476d5378bcdb424266
paulorsilla <paulo.silla@gmail.com>
2015-02-05 13:36:24 +0000

    Tratamento da ViewExpiredSession.

---
+++ src/br/embrapa/cnpso/sigco/model/utils/JsfExceptionHandlerFactory.java
@@ -0,0 +1,19 @@
+package br.embrapa.cnpso.sigco.model.utils;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+public class JsfExceptionHandlerFactory extends ExceptionHandlerFactory {
+	
+	private ExceptionHandlerFactory parent;
+	
+	public JsfExceptionHandlerFactory(ExceptionHandlerFactory parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new JsfExceptionHandler(parent.getExceptionHandler());
+	}
+
+}
